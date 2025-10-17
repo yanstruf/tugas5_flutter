e425666309d561c1a96a4db9b34ec7055aa3c7d1
yanstruf <alamsyahsofyan800@gmail.com>
2025-10-17 09:48:31 +0700

    Update tugas5_flutter.dart

--- tugas5/tugas5_flutter.dart
+++ tugas5/tugas5_flutter.dart
@@ -10,11 +10,39 @@ class StateDay12 extends StatefulWidget {
 class _State2Day12State extends State<StateDay12> {
   bool showName = false;
   bool showFavorite = false;
+  bool tampilDeskripsi = false;
+  bool text = false;
+
   String name = 'Sofyan Alamsyah';
   String favorite = 'Disuka';
+
+  num angka = 0;
+  int angkasatu = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        spacing: 15,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                angka++;
+              });
+            },
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                angka--;
+              });
+            },
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
       appBar: AppBar(title: Text('Halaman Interaksi')),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -74,6 +102,83 @@ class _State2Day12State extends State<StateDay12> {
               ),
             ),
             Text(showFavorite ? 'Disuka' : ''),
+            SizedBox(child: Column(children: [
+                ],
+              )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsetsGeometry.all(2)),
+                const SizedBox(height: 8),
+                const SizedBox(height: 8),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      tampilDeskripsi = !tampilDeskripsi;
+                    });
+                  },
+                  child: Text(
+                    tampilDeskripsi ? 'Sembunyikan' : 'Lihat selengkapnya',
+                    style: const TextStyle(color: Colors.black),
+                  ),
+                ),
+
+                Center(
+                  child: Text(angka.toString()),
+                ), //Manggil floationbutton diatas
+
+                if (tampilDeskripsi)
+                  const Text(
+                    'Aku bukan malas, cuma lagi mode hemat energi',
+                    style: TextStyle(color: Colors.black),
+                  ),
+              ],
+            ),
+            //Inkwell
+            Container(
+              width: 150,
+              height: 150,
+              color: Colors.limeAccent,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: InkWell(
+                    splashColor: Colors.blue,
+                    onTap: () {
+                      print('Kotak diSentuh');
+                      text = !text;
+                      setState(() {});
+                    },
+                    child: Center(child: Text(text ? 'haloo' : '')),
+                  ),
+                ),
+              ),
+            ), //Container inkwell
+
+            SizedBox(height: 100),
+            GestureDetector(
+              onDoubleTap: () {
+                print('Ditekan dua kali');
+                setState(() {
+                  angkasatu += 2;
+                });
+              },
+              onLongPress: () {
+                print('Tahan Lama');
+                setState(() {
+                  angkasatu += 3;
+                });
+              },
+              onTap: () {
+                print('ditekan sekali');
+                setState(() {
+                  angkasatu += 1;
+                });
+              },
+              child: Text('Tekan Aku'),
+            ),
+            Center(child: Text('Jumlahnya : $angkasatu')),
           ],
         ),
       ),
